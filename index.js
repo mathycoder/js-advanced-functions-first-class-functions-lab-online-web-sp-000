17c73bc5127cdf5f64159c195690af3297fd8330
Adam Sarli <adam.sarli@gmail.com>
2019-08-08 13:09:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+// const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(drivers) {
+  const myFunc = function (drivers) {
+    return drivers.slice(0, 2)
+  }
+  return myFunc(drivers)
+}
+
+function returnLastTwoDrivers(drivers) {
+  const myFunc = function (drivers) {
+    return drivers.slice(-2)
+  }
+  return myFunc(drivers)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+//selectingDrivers[0](drivers)
+//= ["Sally", "Bob"]
+
+function createFareMultiplier(int) {
+  return function (fare) {
+    return fare * int
+  }
+}
+//createFareMultiplier(4)
+//= returns the nested function (uninvoked) with a local variable of int = 4
+// createFareMultiplier(4)(10)
+//= 40
+
+function fareDoubler(fare) {
+  const myFunc = createFareMultiplier(2)
+  return myFunc(fare)
+}
+
+function fareTripler(fare) {
+  const myFunc = createFareMultiplier(3)
+  return myFunc(fare)
+}
+
+function selectDifferentDrivers(drivers, func) {
+  return func(drivers)
+}
